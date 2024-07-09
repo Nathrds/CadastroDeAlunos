c39b90f1e32e9600644a0a501bce54c190bcc860
Nathália <nathaliarsantos@live.com>
2024-07-09 15:45:37 -0300

    tela de login com css (inicio)

--- CadAlunos-project/src/App.jsx
+++ CadAlunos-project/src/App.jsx
@@ -1,14 +1,21 @@
 // import { useState } from 'react'
 import './App.css'
-import { LoginForm } from './components/LoginForm'
+// import { LoginForm } from './components/LoginForm'
 
 function App() {
 
   return (
     <>
-      <div>
-      <h2>Seja bem-vindo ao</h2>
-      <LoginForm />
+      <div className='container-login'>
+      <h2 className='h2-login'>Seja bem-vindo(a) ao</h2>
+        <div className='logo-name'>
+          <h1><span>Cad</span> Aluno</h1>
+        </div>
+        <p>Faça seu login</p>
+        <div className='info-login'>
+          {/* <LoginForm /> */}
+          <p>Não tem uma conta? <a href="">Cadastre-se</a></p>
+        </div>
       </div>
     </>
   )
